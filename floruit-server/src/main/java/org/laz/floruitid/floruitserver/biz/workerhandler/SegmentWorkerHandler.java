81a19f1fa248f97c77162388c3c58b6b2a7ada8c
刘安正 <424688659@qq.com>
2025-12-19 12:39:21 +0800

    feat(server): 完善SegmentWorkerHandler的异常处理逻辑

--- floruit-server/src/main/java/org/laz/floruitid/floruitserver/biz/workerhandler/SegmentWorkerHandler.java
+++ floruit-server/src/main/java/org/laz/floruitid/floruitserver/biz/workerhandler/SegmentWorkerHandler.java
@@ -1,6 +1,7 @@
 package org.laz.floruitid.floruitserver.biz.workerhandler;
 
 import com.lmax.disruptor.WorkHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.laz.floruitid.floruitserver.biz.idprovider.IdProvider;
 import org.laz.floruitid.floruitserver.biz.idprovider.SegmentIdProvider;
 import org.laz.floruitid.floruitserver.modle.event.AbstractEvent;
@@ -9,17 +10,27 @@ import org.laz.floruitid.floruitserver.modle.proto.resp.RespData;
 /**
  * Disruptor消费者
  */
+@Slf4j
 public class SegmentWorkerHandler implements WorkHandler<AbstractEvent> {
 
     private final IdProvider provider = new SegmentIdProvider();
 
     @Override
     public void onEvent(AbstractEvent event) throws Exception {
-        long id = provider.getId();
-        event.getCtx().writeAndFlush(RespData.newBuilder()
-                .setId(id)
-                .setSuccess(true)
-                .setMessage("Success")
-                .build());
+        try {
+            long id = provider.getId();
+            event.getCtx().writeAndFlush(RespData.newBuilder()
+                    .setId(id)
+                    .setSuccess(true)
+                    .setMessage("Success")
+                    .build());
+        } catch (Exception e) {
+            log.error("Segment Provider Error: ", e);
+            event.getCtx().writeAndFlush(RespData.newBuilder()
+                    .setId(0L)
+                    .setSuccess(false)
+                    .setMessage("Error")
+                    .build());
+        }
     }
 }
